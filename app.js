d45371eeffcbed23f858f29f57c417d9743a270e
Cryptofundamentalism <100459866+Cryptofundamentalism@users.noreply.github.com>
2022-08-08 13:57:48 -0700

    Update app.js
    Signed-off-by: Cryptofundamentalism <100459866+Cryptofundamentalism@users.noreply.github.com>

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ fetch('https://api.github.com/repos/bitcoin/bitcoin/stats/commit_activity')
 .then(res => res.json())
 .then((res) => {
   const data = res.data;
-  getElement('0/total').innerHTML =  'firstweek: ' + data.name;
-  getElement('symbol').innerHTML = 'Symbol: ' + data.symbol;
+  getElement('0').innerHTML =  'firstweek: ' + data.name;
+  getElement('1').innerHTML = 'secondweek: ' + data.symbol;
   getElement('rank').innerHTML = 'Rank: ' + data.rank;
   getElement('price').innerHTML = 'Price: ' + data.quotes.USD.price;
   // do the rest here
