d8c58367ae8e77d1d8f55a1b8504cfec7992dbac
Cryptofundamentalism <100459866+Cryptofundamentalism@users.noreply.github.com>
2022-08-08 14:26:43 -0700

    Update app.js
    Signed-off-by: Cryptofundamentalism <100459866+Cryptofundamentalism@users.noreply.github.com>

--- app.js
+++ app.js
@@ -3,3 +3,5 @@ fetch(https://api.github.com/repos/bitcoin/bitcoin/stats/commit_activity)
   .then(response => response.json())
   // use (display) the JSON data
   .then(data => console.log(data))
+
+data.result[0].total
