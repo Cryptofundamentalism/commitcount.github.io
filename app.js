02f086561161f952467023dc46e7491626dea898
Cryptofundamentalism <100459866+Cryptofundamentalism@users.noreply.github.com>
2022-08-11 21:06:02 -0700

    Update and rename app.js to btc.js
    Signed-off-by: Cryptofundamentalism <100459866+Cryptofundamentalism@users.noreply.github.com>

--- app.js
+++
@@ -1,7 +0,0 @@
-fetch(https://api.github.com/repos/bitcoin/bitcoin/stats/commit_activity)
-  // get the JSON data
-  .then(response => response.json())
-  // use (display) the JSON data
-  .then(data => console.log(data))
-
-data.result[0].total
